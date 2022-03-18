30f8830965435007c34cac9eb72df996a9139fec
awaisanwar544 <awaisanwar544@gmail.com>
2022-03-19 01:10:26 +0500

    initial setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import TodoContainer from './components/TodoContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <main className="main">
+        <TodoContainer />
+      </main>
+    </div>
+  );
+}
+
+export default App;
